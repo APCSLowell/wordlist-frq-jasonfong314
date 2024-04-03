f38e439d57b19c7ac06202051e2377562c206a7b
J <143027556+jasonfong314@users.noreply.github.com>
2024-04-03 12:08:30 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -12,7 +12,7 @@ public class WordList
   {
     int sum = 0;
     for (int i=0; i< myList.size();i++){
-      if (myList.get(i).length()==len);
+      if (myList.get(i).length()==len){
       sum = sum +1;
     }
   }
